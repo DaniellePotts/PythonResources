791ffde73e6c599e119a0e865e7e7e8a62cdbc75
~Danielle <danielle.potts@accenture.com>
2021-10-13 16:01:55 +0100

    Update find_data.py

--- find_data.py
+++ find_data.py
@@ -1,5 +1,5 @@
 def spot_term(texts, term):
-  return [t for t in texts if term.lower() in t]
+  return [t for t in texts if term.lower() in t.lower()]
 
 def term_exists(text, term):
   return term.lower() in text
